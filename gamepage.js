8fd9fe9c6ebdfcf1f9782eb85fed98a0a647e04a
blessjeno <86838748+blessjeno@users.noreply.github.com>
2022-09-24 20:00:12 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,50 @@
+// random value generated
+var y = Math.floor(Math.random() * 10 + 1 );
+
+
+
+function submit()
+{  
+// counting the number of guesses
+// made for correct Guess
+var x = document.getElementById("guessField").value;
+if(x == y)
+{
+    alert("CONGRATULATIONS!!!"+playername+" YOU GUESSED IT RIGHT IN "
+    + guess + " GUESS ");
+    guess= 1;
+}
+
+// function for number guessed by user     
+else if (x > y)
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
+else
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A GREATER NUMBER")
+} 
+}
+function playAgain(){
+  var y = Math.floor(Math.random() * 10 + 1);
+}
+
+var x = document.getElementById("guessField");
+if(x == y)
+{
+    alert("CONGRATULATIONS!!!" +playername+" YOU GUESSED IT RIGHT IN "
+    + guess + " GUESS ");
+    guess= 1;
+}
+else if(x > y)
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
+else if(x < y)
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A GREATER NUMBER");
+}
